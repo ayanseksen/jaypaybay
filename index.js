661ee288957acf6aeab25ef9b0911ccdbc7dbd4c
ayanseksen <56995222+ayanseksen@users.noreply.github.com>
2019-10-27 14:32:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-require('dotenv').config();
 
-const Telegraf = require('telegraf');
-const transformString = require('./transform-string.js');
 
-const bot = new Telegraf(process.env.JAYPAYBOT_TOKEN);
-bot.command('/start', ctx => {
-  ctx.reply(
-    'Добро пожаловать в бот, который поможет вам транслитерировать казахские слова на казахскую латиницу!\nПросто напишите ему любой текст и он проведёт транслитерацию.\nАвтор: @drugoi\nОтдельная благодарность: @talgautb'
-  );
-});
+const TelegramBot = require('node-telegram-bot-api')
 
-bot.on('text', ({ message, replyWithMarkdown }) => {
-  const reply = `${transformString(message.text)}`;
-  replyWithMarkdown(reply);
-});
+const TOKEN = '937827713:AAHewPehu_Nayj9ORu1NGBv2u3iUSNJzEcw'
 
-bot.on('inline_query', ({ inlineQuery, answerInlineQuery }) => {
-  if (inlineQuery.query && inlineQuery.query.length) {
-    const answer = transformString(inlineQuery.query);
-    answerInlineQuery([
-      {
-        id: '1',
-        type: 'article',
-        title: answer,
-        input_message_content: {
-          message_text: `${answer}`,
-          parse_mode: 'Markdown',
-          disable_web_page_preview: true
-        }
-      }
-    ]);
-  }
-});
+const bot = new TelegramBot(TOKEN, {polling:true})
 
-bot.startPolling();
-
-module.exports = bot;
-
+bot.on('message', msg => {
+	bot.sendMessage(msg.chat.id, 'Hello from JayPay, bot says: "Hi, ${msg.from.first_name}"')
+})
