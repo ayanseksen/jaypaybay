8a3e7f550e39eff484f613e466a7a905254928e9
jumissiz <>
2019-10-27 14:14:47 +0600

    23

---
+++ test.js
@@ -0,0 +1,40 @@
+/* eslint-disable quotes */
+
+import test from 'ava';
+
+const transformString = require('./transform-string');
+
+test('string transforming lowercase', t => {
+  t.is(
+    transformString('қанағаттандырылмағандықтарыңыздан'),
+    "qanag'attandyrylmag'andyqtaryn'yzdan"
+  );
+});
+
+test('string transforming capitalize', t => {
+  t.is(
+    transformString('Қанағаттандырылмағандықтарыңыздан'),
+    "Qanag'attandyrylmag'andyqtaryn'yzdan"
+  );
+});
+
+test('string transforming uppercase', t => {
+  t.is(
+    transformString('ҚАНАҒАТТАНДЫРЫЛМАҒАНДЫҚТАРЫҢЫЗДАН'),
+    "QANAG'ATTANDYRYLMAG'ANDYQTARYN'YZDAN"
+  );
+});
+
+test('full alphabet', t => {
+  t.is(
+    transformString('аәбвгғдеёжзийкқлмнңоөпрстуұүфхһцчшщъыіьэюя'),
+    "aa'bvgg'dejojzi'i'kqlmnn'oo'prsty'uu'fhhtsc's's'yiei'y'i'a"
+  );
+});
+
+test('full alphabet capitalize', t => {
+  t.is(
+    transformString('АӘБВГҒДЕЁЖЗИЙКҚЛМНҢОӨПРСТУҰҮФХҺЦЧШЩЪЫІЬЭЮЯ'),
+    "AA'BVGG'DEJoJZI'I'KQLMNN'OO'PRSTY'UU'FHHTsC'S'S'YIEI'y'I'a"
+  );
+});
