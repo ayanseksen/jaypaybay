9f7a81b7799759972ac7d389127a320d0baa4501
ayanseksen <56995222+ayanseksen@users.noreply.github.com>
2019-10-27 14:11:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const TelegramBot = require('node-telegram-bot-api')
-
 const TOKEN = '937827713:AAHewPehu_Nayj9ORu1NGBv2u3iUSNJzEcw'
 
-const bot = new TelegramBot(TOKEN, {polling:true})
+require('dotenv').config();
 
+const Telegraf = require('telegraf');
+const transformString = require('./transform-string.js');
+
+const bot = new Telegraf(TOKEN, {polling:true});
 
 bot.command('/start', ctx => {
   ctx.reply(
