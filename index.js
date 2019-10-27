68a60bbbafb34ba28a8399c91d1e4ba2dcd138f3
ayanseksen <56995222+ayanseksen@users.noreply.github.com>
2019-10-27 13:31:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const TOKEN = '937827713:AAHewPehu_Nayj9ORu1NGBv2u3iUSNJzEcw'
 
 const bot = new TelegramBot(TOKEN, {polling:true})
 
-bot.on('message', msg.chat.id, 'Hello from JayPay, bot says; "Salem, ${msg.from.first_name}"')
+bot.on('message', msg.chat.id, 'Hello from JayPay, bot says; "Hi, ${msg.from.first_name}"')
 
